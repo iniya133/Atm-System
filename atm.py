b61a05b02c621846ee5527df6873e49afd36c8d3
Iniya Rajasekhar <56218662+iniya133@users.noreply.github.com>
2021-02-04 18:10:05 +0530

    Add files via upload

---
+++ atm.py
@@ -0,0 +1,29 @@
+print("Welcome to the fastest Banking System!")
+ac_num = int(input("Enter your Registed Bank Account Number: "))
+pin_num = int(input("Enter your pin number: "))
+balance = 25000
+if (ac_num == 2) : 
+    if( pin_num == 8):
+        print("Welcome, Please choose your transaction of your choice: ")
+        print("1.Balance Enquiry")
+        print("2.Deposit")
+        print("3.Withdrawl")
+        choice =int(input("We are waiting for your options... "))
+        if(choice == 1):
+            print("Balance is:",balance)
+        elif(choice == 2):
+            deposit = int(input("Enter amount you want to deposit: "))
+            balance = balance+deposit
+            print("Balance is:",balance)
+        elif(choice == 3):
+            withdraw = int(input("Enter amount you want to deposit: "))
+            balance = balance-withdraw
+            print("Balance is:",balance)
+        else:
+            print("Invalid Input!")            
+
+    else:
+        print("Wrong pin number!")
+
+else:
+    print("Account does not exist!!")
